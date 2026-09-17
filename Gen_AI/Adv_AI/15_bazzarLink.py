8b20fcb14c951f75771f6ef6febba3b43b3c1541
sudheer77000 <sudheergundra@gmail.com>
2026-09-17 21:57:19 +0400

    17-Sep-2026-V1

---
+++ Gen_AI/Adv_AI/15_bazzarLink.py
@@ -0,0 +1,18 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+apikey = os.getenv("BAZAAR_LINK_API_KEY")
+
+llm = ChatOpenAI(
+    model="auto:free",
+    base_url="https://api.bazaarlink.ai/v1",
+    api_key=apikey,
+    temperature=0
+)
+
+response = llm.invoke("Explain LangChain in simple terms")
+print(response)
+print("===" * 25)
+print(response.content)
